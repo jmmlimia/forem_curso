6b2b48d49ef65b527240923a8c2b0fb7f1bebf56
jmmlimia <jmmlimia@jmmlimia.me>
2025-02-07 20:15:58 +0100

    Segundo commit

--- Proyecto 001/Main.java
+++ Proyecto 001/Main.java
@@ -1,5 +1,11 @@
 public class Main {
-        public static void main(string[] args) {  
-                System.out.println("¡Hola, mundo!");
-        }
+     public static void main(string[] args) {  
+        System.out.println("¡Hola, mundo!");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+    }
 }
